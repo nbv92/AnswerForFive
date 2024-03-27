9e3c8e00b8edc6d124e6bda57d09009b3271c0e3
Timofey <timofey.sharipov@gmail.com>
2024-03-27 19:09:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        @Override
+
+                public boolean equals (Object o) {
+            if(this == o) return true;
+            if(!(o))
+        }
+    }
+}
